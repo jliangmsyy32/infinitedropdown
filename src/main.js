cc29bc84c1379229b2c74b63160e01e713088e60
lwxyfer <lwxyfer@gmail.com>
2016-07-19 16:27:07 +0800

    achieve all functions

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 'use strict';
 import { data } from './data';
-import { loop,create,clean } from './utils';
+import { loop,create,clean,getIndex,dataCheck } from './utils';
 import { ChineseDistricts as CD } from './citydata';
 
 function idd({
@@ -8,57 +8,63 @@ function idd({
   data = false, // required.指定载入的数据，不提供AJAX的方法； 提供多个实例。
   init = 1, // required
   showTooltip = true, // optional,是否显示指示，无占位符直接获取第一个数据
-  tooltip = '设置提示信息', // optional, 替换默认的提示信息
+  tooltip = '请设置提示信息', // optional, 替换默认的提示信息;多个提示信息就循环数组进行替换
   defaultValue = false, // optional，以数组给出接入，
   className = '', // optional, 样式接口
-  callback = '', // optional, 设置回调函数，每次选择后触发回调函数。
+  callback = false, // optional, 设置回调函数，每次选择后触发回调函数。
 }) {
-  // console.log(node,data,defaultValue);
+
   let iddNode = document.querySelector(selector);
-  let allSelect = [];
+  let allSelect = []; // for storing selected data
 
-/**
- * [get Index ]
- * @param  {[NUmber/String]} code [the code of value]
- * @param  {[type]} dataset  [description]
- * @param  {[type]} data  [description]
- * @return {[type]}       [description]
- */
-  function getIndex(code,dataset,data) {
-    let a = 0;
-    console.log(code,' code')
-    for( let items in data[dataset]) {
-        a ++;
-        console.log(items)
-        if(items == code ) {
-            console.log('found')
-            return a;
-        }
-    }
-    console.log('no data found')
-    return false;
-  }
-
-  let first = loop(init,data);
-  let firstIndex;
-  if(defaultValue ) {
-    firstIndex = getIndex(defaultValue[0],init,data)
-  }
+  let first = loop(init,data); // get initialization
+  let initNode;  // init first node
   first.unshift([tooltip,1]);
-  // allSelect.push(init);
-  let initNode = create(init,first,iddNode);
-  initNode.selectedIndex = firstIndex;
-  console.log(firstIndex)
 
-  if(defaultValue) {
-    defaultValue.forEach((items)=>{
-        let dataBack = loop(items,data)
-        let nodeBack = create('init',dataBack,iddNode)
-        let index = getIndex(items,)
+  if(!defaultValue && dataCheck(first,'array')) {
+    initNode = create(init,first,iddNode);  // 设置了默认值后是不同的
+  } else if( dataCheck(defaultValue,'array') ) {
+      // 有默认值时， 初始化
+      let firstIndex;
+      initNode = create(defaultValue[0],first,iddNode);
+      firstIndex = getIndex(defaultValue[0],init,data);
+      initNode.selectedIndex = firstIndex;
+      let values = {};
+      values.value = defaultValue[0];
+      values.text = initNode.options[firstIndex].text;
+      allSelect.push(values);
+
+      // 多个默认值的循环
+      defaultValue.forEach((items,i,arr)=>{
+        let dataBack = loop(items,data);
+        dataBack.unshift([tooltip,1]);
+        let nodeBack;
+        let index;
+        // let nodeBack = create('init',dataBack,iddNode);
+        let nextIs = arr[i+1];
+
+        let values = {};
+
+        if(nextIs) {
+          index = getIndex(arr[i+1],items,data);
+          nodeBack = create(nextIs,dataBack,iddNode);
+          nodeBack.selectedIndex = index;
+
+          values.value = nextIs;
+          values.text = nodeBack.options[index].text;
+          allSelect.push(values);
+          console.log(allSelect)
+        } else {
+          create('init',dataBack,iddNode);
+        }
     })
   }
 
-
+/**
+ * [when selected, trigger render function]
+ * @param  {[type]} event [description]
+ * @return {[Array]}       [the selects's data]
+ */
   function render(event) {
     let target = event.target || event.srcElement;
     if(target.nodeName.toUpperCase() === 'SELECT') {
@@ -66,49 +72,72 @@ function idd({
       let value = target.options[index].value;
       let text = target.options[index].text;
       console.log('当前选择: ',text);
+
       let values = {};
       values.value = value;
       values.text = text;
+
       if( target.name !== 'init') {
         clean(target.name,iddNode,allSelect);
       }
+
       target.name = value;
       allSelect.push(values);
+
       let dataset = loop(value,data);
-      if( dataset.length ) {
-        dataset.unshift([tooltip,1]);
-        create('init',dataset,iddNode)
-      }
-      if( typeof callback) {
+      dataset.unshift([tooltip,1]);
+      create('init',dataset,iddNode)
+
+      // 回调函数接口
+      if( callback) {
         console.log(callback)
         callback();
       }
     }
+
     console.log('已选择数据: ',allSelect)
   }
   // 事件监听，通过change事件触发数据的刷新
   iddNode.addEventListener('change',render,false);
-  return allSelect;
+
+  // 暴露公共接口
+  return {
+    data:allSelect,
+  }
 }
 
-// trigger event
-// var a = new idd({
-//   selector:'.am-idd',
-//   data: CD,
-//   init:86,
-//   showTooltip:true,
-//   tooltip:'-- 你说了算 --'
-// })
+// ================================================================
 
+// trigger event
+var a = new idd({
+  selector: '.am-idd',
+  data: CD,
+  init: 86,
+  showTooltip: true,
+  defaultValue: [510000,510100],
+  tooltip: '-- 你说了算 --'
+})
 
 idd({
   selector:'.testNode',
   data: data,
   init: 1,
   showTooltip: true,
-  defaultValue: [10],
-  callback: foo,
-})
-function foo() {
-  console.log('111111')
+  defaultValue: [10,103],
+});
+
+idd({
+  selector:'.noDefault',
+  data: data,
+  init: 1,
+  showTooltip: true,
+});
+
+var getData = document.getElementById('getData')
+getData.onclick = function() {
+  let allData =''
+  for(let i=0; i<a.data.length; i++) {
+    allData += a.data[i].value + ' - ' + a.data[i].text + ' \n'
+  }
+  alert(allData)
 }
--- src/utils.js
+++ src/utils.js
@@ -14,18 +14,22 @@ export function loop(value,data) {
   }
 
 /**
- * [create description]
+ * [judge content, create description]
  * @param  {[type]} name    [description]
  * @param  {[type]} options [description]
  * @param  {[type]} node    [description]
  * @return {[type]}         [return current node just created]
  */
-export function create(name,options,node) {
+export function create(name,optionsContent,node) {
+  if(optionsContent.length < 2) {
+    console.log('NO INCOMING DATA');
+    return false;
+  }
   let select = document.createElement('select');
   let content = '';
   select.name = name;
-  for(let i=0; i<options.length; i++) {
-    content += `<option value=${options[i][1]}>${options[i][0]}</option>`
+  for(let i=0; i<optionsContent.length; i++) {
+    content += `<option value=${optionsContent[i][1]}>${optionsContent[i][0]}</option>`
   }
   select.innerHTML = content;
   node.appendChild(select);
@@ -41,9 +45,7 @@ export function create(name,options,node) {
 export function clean(name,node,array) {
   // let a = document.querySelector(`select[name=${name}]`);
   let last = node.lastChild;
-
-    array.pop()
-
+  array.pop();
   while( last.name !== name && last.nodeType === 1) {
     node.removeChild(last);
     if( last.name !=='init' ) {
@@ -51,6 +53,43 @@ export function clean(name,node,array) {
     }
     last = node.lastChild;
   }
+  return true;
 }
 
 // two ways to delete dom
+
+/**
+ * detect if the value is definied and type is the same as we give
+ * @param  {[**]} value [data]
+ * @param  {[String]} type  [the data type you want to check]
+ * @return {[Boolean]}
+ */
+export function dataCheck(value,type) {
+  let clas = Object.prototype.toString.call(value).slice(8, -1).toLowerCase();
+  console.log(clas,'你的数据类型');
+  if( value && clas === type.toLowerCase() ) {
+    return true;
+  } else {
+    console.log('默认值设置错误');
+    return false;
+  }
+}
+
+/**
+ * [get Index ]
+ * @param  {[NUmber/String]} code [the code of value]
+ * @param  {[type]} dataset  [description]
+ * @param  {[type]} data  [description]
+ * @return {[Number]}       [return the index number]
+ */
+export function getIndex(code,dataset,data) {
+  let a = 0;
+  for( let items in data[dataset]) {
+      a ++;
+      if(items == code ) {
+          return a;
+      }
+  }
+  console.log('no data found')
+  return false;
+}
